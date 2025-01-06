1019c94016f5fab5bc81417bac16f8e74bfd388a
bling-yshs <377178599@qq.com>
2025-01-06 13:40:31 +0800

    feat: 添加插件图标支持
    - 创建OpenInCursorIcons类用于管理插件图标
    - 添加Action图标的亮色和暗色主题支持
    - 更新plugin.xml中的图标引用

---
+++ src/main/java/com/github/blingyshs/openincursor/OpenInCursorIcons.kt
@@ -0,0 +1,8 @@
+package com.github.blingyshs.openincursor
+
+import com.intellij.openapi.util.IconLoader
+
+object OpenInCursorIcons {
+    @JvmField
+    val OpenInCursorAction = IconLoader.getIcon("/icons/openInCursorAction.svg", OpenInCursorIcons::class.java)
+}
