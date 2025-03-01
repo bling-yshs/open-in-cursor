0601cc119498fd60d74129b780f1b819fa5f1f5b
bling-yshs <377178599@qq.com>
2025-03-01 20:04:24 +0800

    fix: 修复图标丢失

---
+++ src/main/java/com/github/blingyshs/openincursor/OpenInCursorIcons.kt
@@ -0,0 +1,9 @@
+package com.github.blingyshs.openincursor
+
+import com.intellij.openapi.util.IconLoader
+
+@Suppress("unused")
+object OpenInCursorIcons {
+    @JvmField
+    val OpenInCursorAction = IconLoader.getIcon("/icons/openInCursorAction.svg", OpenInCursorIcons::class.java)
+}
